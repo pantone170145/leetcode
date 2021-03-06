f6ebf2912506f26991991a859b0305d94bb83b56
pantone170145 <pantone170145@gmail.com>
2021-03-06 20:01:51 +0900

    Algorithms 30 検討中

---
+++ Algorithms/30.substring-with-concatenation-of-all-words.go
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=30 lang=golang
+ *
+ * [30] Substring with Concatenation of All Words
+ */
+
+// @lc code=start
+func findSubstring(s string, words []string) []int {
+	// すべての組み合わせを事前につくるのは微妙
+	// wordsの完全一致を探す?
+}
+
+// @lc code=end
+
