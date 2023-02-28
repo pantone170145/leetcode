83b16110e56517e75638c6b46821fe273b95a9a4
pantone170145 <pantone170145@gmail.com>
2023-03-01 01:43:43 +0900

    217

---
+++ Algorithms/217. Contains Duplicate /solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hash = set()
+        for i in nums:
+            if i in hash:
+                return True
+            hash.add(i)
+
+        return False
