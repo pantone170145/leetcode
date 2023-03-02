a947519d2708363e9cc05ccb8808582340951fc4
pantone170145 <pantone170145@gmail.com>
2023-03-02 15:09:27 +0900

    update

--- Algorithms/347. Top K Frequent Elements/solution.py
+++ Algorithms/347. Top K Frequent Elements/solution.py
@@ -1,7 +1,5 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        d = defaultdict(int)
-        for n in nums:
-            d[n] += 1
-        items = sorted(d.items(), reverse=True, key=lambda x: x[1])
-        return list(map(lambda x: x[0], items))[:k]
+        d = dict(Counter(nums))
+        items = dict(sorted(d.items(), reverse=True, key=lambda x: x[1]))
+        return list(items.keys())[:k]
