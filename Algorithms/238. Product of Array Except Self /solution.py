2984fd1b2e91522b8f4801ad1a429d08de81d39c
pantone170145 <pantone170145@gmail.com>
2023-03-08 22:34:55 +0900

    修正

--- Algorithms/238. Product of Array Except Self /solution.py
+++ Algorithms/238. Product of Array Except Self /solution.py
@@ -4,34 +4,15 @@ class Solution:
         if len(nums) <= 1:
             return nums
 
-        self.cache = {}
-        result = []
+        # 1で初期化
+        left = [1] * len(nums)
 
-        # i番目の要素を除いたリストを作成し、
-        # そのリストの要素の積を計算する
-        for i, n in enumerate(nums):
-            nums_without_i = nums[:i] + nums[i + 1:]
-            result.append( self.memoize(nums_without_i))
+        for i in range(1, len(nums)):
+            left[i] = left[i - 1] * nums[i - 1]
 
-        return result
+        right = 1
+        for i in range(len(nums) - 1, -1, -1):
+            left[i] *= right
+            right *= nums[i]
 
-    def memoize(self, nums: List[int]) -> int:
-        # convert list to string
-        str_nums = [str(n) for n in nums]
-
-        # concatenate strings with '-' nums
-        key = '-'.join(str_nums)
-        print(f'key: {key}')
-
-        # キャッシュにkeyが存在するか
-        if key in self.cache:
-            return self.cache[key]
-
-        # numsが>=2の場合、計算しキャッシュに保持する
-        if len(nums) >= 2:
-            self.cache[key] = self.memoize(nums[1:]) * nums[0]
-            return self.cache[key]
-
-        # numsが1の場合、キャッシュに保持する
-        self.cache[key] = nums[0]
-        return self.cache[key]
+        return left
